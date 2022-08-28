5a4709466e649bc44aa6aa1b98c187afc685138d
zz <zzhe0042@student.monash.edu>
2022-08-28 21:06:46 +1000

    Create basic layout for homepage

---
+++ src/components/HomeButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <button>
+        <slot></slot>
+        <img src="@/assets/images/IconButtonRight.png" alt="icon">
+    </button>
+</template>
+
+<style lang="postcss" scoped>
+button{
+    display: flex;
+    user-select: none;
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 36px;
+    padding: 4px 10px;
+    border-radius: 40px;
+    border: 1px black solid;
+    box-sizing: border-box;
+    font-family: 'Average';
+    text-align: middle;
+    gap: 8px;
+    background-color: white;
+}
+img{
+    display: block;
+    pointer-events: none;
+    width: 24px;
+    margin:auto 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import HomeButton from '../components/HomeButton.vue';    
+</script>
+
+<template>
+    <section class="sed">
+        <img class="text-1" alt="Sedentary Lifestyle" src="@/assets/images/HomeText1.svg" />
+        <img class="image-1" alt="Sitting at work" src="@/assets/images/HomeImage1.svg" />
+        <HomeButton>Learn More</HomeButton>
+    </section>
+    <section class="tool">
+        <img class="text-2" alt="Improve Life Standards" src="@/assets/images/HomeText2.svg" />
+        <img class="image-2" alt="Stand up" src="@/assets/images/HomeImage2.svg" />
+        <HomeButton>Explore Tool</HomeButton>
+    </section>
+</template>
+
+<style lang="postcss" scoped>
+.sed{
+    position: relative;
+    width: 100%;
+}
+.text-1 {
+    position: relative;
+    width: 50vw;
+}
+
+.text-2 {
+    width: 50vw;
+}
+</style>
