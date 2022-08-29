6376c3d95b3db506da196c0d7d41ce39f2ca3320
zz <zzhe0042@student.monash.edu>
2022-08-29 21:58:28 +1000

    Add a Menu button when screen size is smaller than 1024px

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <RouterLink class="home" to="/">
-      <img class="logo" alt="logo" src="@/assets/logo.svg"/>
-    </RouterLink>
-    
-    <nav>
-      <RouterLink to="/sedentary-lifestyle">Sedentary Lifestyle</RouterLink>
-      <RouterLink to="/physical-ergonomics">Physical Ergonomics</RouterLink>
-      <RouterLink to="/physical-activities">Physical Activities</RouterLink>
-      <RouterLink to="/dietary-plan">Dietary Plan</RouterLink>
-      <RouterLink to="/alert-reminder">Alert Reminder</RouterLink>
-    </nav>
-
-    <RouterLink class="new-user" to="/become-new-user">
-      Become new user
-    </RouterLink>
-  </header>
+  <Header></Header>
   
   <RouterView v-slot="{ Component, route }">
     <Transition name="view">
@@ -39,59 +24,5 @@ import { RouterLink, RouterView } from 'vue-router'
 .view-enter-from,
 .view-leave-to {
   opacity: 0;
-}
-
-header{
-  display: flex;
-  height: 80px;
-  align-items: center;
-  & .logo{
-    height: 80px;
-    max-height: 100px;
-    min-height: 80px;
-    margin-left: 20px;
-  }
-  a.home{
-    display:block;
-    line-height: 0;
-  }
-  & nav>a, & a.new-user{
-    color: var(--color-text);
-    text-decoration: none;
-    font-size: 15px;
-    font-weight: 600;
-    display: inline-block;
-    padding:16px;
-    border-radius: 8px;
-    &:hover{
-      background-color: var(--color-hover);
-    }
-    &:active{
-      background-color: var(--color-press);
-    }
-    &:focus{
-      box-shadow: var(--color-focus);
-      z-index: 1;
-    }
-    &.router-link-active{
-      background-color: var(--color-active);
-      &:hover{
-        background-color: var(--color-active-hover);
-      }
-      &:active{
-        background-color: var(--color-active-press);
-      }
-    }
-  }
-  & nav{
-    flex: 1 0 auto;
-    display: flex;
-    justify-content: center;
-    /* bp: ? - 1024 */
-    
-  }
-  & .new-user{
-    margin-right:20px;
-  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <RouterLink class="home" to="/">
+      <img class="logo" alt="logo" src="@/assets/logo.svg" />
+    </RouterLink>
+
+    <nav>
+      <RouterLink to="/sedentary-lifestyle">Sedentary Lifestyle</RouterLink>
+      <RouterLink to="/physical-ergonomics">Physical Ergonomics</RouterLink>
+      <RouterLink to="/physical-activities">Physical Activities</RouterLink>
+      <RouterLink to="/dietary-plan">Dietary Plan</RouterLink>
+      <RouterLink to="/alert-reminder">Alert Reminder</RouterLink>
+    </nav>
+
+    <RouterLink class="new-user" to="/become-new-user">
+      Become new user
+    </RouterLink>
+    <button class="menu-button">
+      <img src="@/assets/images/IconMenu.svg" alt="">
+    </button>
+  </header>
+</template>
+
+<style lang="postcss" scoped>
+header {
+  display: flex;
+  height: 80px;
+  align-items: center;
+  justify-content: space-between;
+  @media(width <1024px) {
+    height:64px;
+  }
+
+  & .home {
+    line-height: 0;
+
+    & .logo {
+      height: 80px;
+      margin-left: 20px;
+      @media(width <1024px) {
+        height:64px;
+      }
+    }
+
+  }
+
+  & nav>a,
+  & a.new-user {
+    /** button-like links */
+    color: var(--color-text);
+    text-decoration: none;
+    font-size: 15px;
+    font-weight: 600;
+    padding: 16px;
+    border-radius: 8px;
+    height: 56px;
+
+    &:hover {
+      background-color: var(--color-hover);
+    }
+
+    &:active {
+      background-color: var(--color-press);
+    }
+
+    &:focus {
+      box-shadow: var(--color-focus);
+    }
+
+    &.router-link-active {
+      /* active state of link */
+      background-color: var(--color-active);
+
+      &:hover {
+        background-color: var(--color-active-hover);
+      }
+
+      &:active {
+        background-color: var(--color-active-press);
+      }
+    }
+  }
+
+  & nav {
+    /* >=1024px */
+    display: flex;
+    @media(width < 1024px) {
+      display: none;
+    }
+  }
+
+  & .new-user {
+    margin-right: 20px;
+
+    @media(width < 1024px) {
+      display: none;
+    }
+  }
+
+  & .menu-button {
+    display: none;
+    border: 0;
+    padding: 0;
+    line-height: 0;
+
+    &:hover {
+      background-color: var(--color-hover);
+    }
+
+    &:active {
+      background-color: var(--color-press);
+    }
+
+    &:focus {
+      box-shadow: var(--color-focus);
+    }
+
+    @media(width <1024px) {
+      display: block;
+      width: 64px;
+      height: 64px;
+      background-color: white;
+    }
+  }
+}
+</style>
