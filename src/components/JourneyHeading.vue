3ea72a1bf82f9b41ce9fd590107208a833ffb270
zz <zzhe0042@student.monash.edu>
2022-08-30 13:45:11 +1000

    The heading should remain the same size for screens larger than 1440px

--- src/components/JourneyHeading.vue
+++ src/components/JourneyHeading.vue
@@ -10,7 +10,7 @@ h1{
     display: inline-block;
     font-family: 'kaisei decol';
     width: 100%;
-    font-size: calc(2vw);
+    font-size: 32px;
     padding: 20px 40px 20px 60px;
     background-color: rgba(134, 127, 127, 0.35);
     border-radius: 0 48px 48px 0;
