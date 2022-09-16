9d57128a888ae76d3fb7884af4309eb9fa0ff8d0
zz <zzhe0042@student.monash.edu>
2022-09-16 11:35:31 +1000

    Add basic alert reminder

--- src/views/AlertReminderView.vue
+++ src/views/AlertReminderView.vue
@@ -1,9 +1,51 @@
 <script setup lang="ts">
-import JourneyLayout from "../components/JourneyLayout.vue";
+// @ts-nocheck
+import Toggle from '@vueform/toggle'
+import '@vueform/toggle/themes/default.css'
+import { ref, watch } from 'vue';
+
+const period = ref<string>('20')
+const toggle = ref<boolean>(false)
+let intervalId = 0
+// Notification.requestPermission()
+
+watch([period, toggle], () => {
+  clearInterval(intervalId)
+  if (toggle.value){
+    const ms = parseInt(period.value) * 60 * 1000
+    intervalId = setInterval(sendNotification, ms)
+    Notification.requestPermission()
+  }
+})
+
+function sendNotification(){
+  new Notification(
+    "Time to drink water", 
+    {body: `Remind you to drink water at every ${period.value} minutes`}
+  )
+}
 </script>
     
 <template>
-<JourneyLayout></JourneyLayout>
+  <section class="flex flex-col justify-center gap-16 my-16">
+    <h1 class="text-4xl font-sans mx-auto">
+      Alert Reminder Settings
+    </h1>
+
+    <ul class="w-[58.25rem] mx-auto">
+      <li class="flex flex-row p-8 bg-white">
+        <span>
+          Remind you to drink water at every:
+        </span>
+        <select v-model="period">
+          <option value="20">20 Minutes</option>
+          <option value="40">40 Minutes</option>
+          <option value="60">60 Minutes</option>
+        </select>
+        <Toggle class="ml-auto" v-model="toggle"></Toggle>
+      </li>
+    </ul>
+  </section>
 </template>
     
 <style lang="postcss" scoped>
