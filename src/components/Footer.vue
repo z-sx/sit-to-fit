f5b5d6dad6626a68994f5efc4768f80115efb32b
zz <zzhe0042@student.monash.edu>
2022-09-20 05:13:02 +1000

    add copyright to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
 <footer class="bg-zinc-300">
   <div class="h-32 max-w-screen-lg mx-auto p-8 flex justify-end">
     <span class="self-end">
-      Copyright 2022
+      Copyright © 2022 Sittofit
     </span>
   </div>
 </footer>
