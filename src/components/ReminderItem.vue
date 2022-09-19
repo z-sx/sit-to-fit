ceb69de0ac92f6c1b8d0bd4ffbe0e02d89ad254b
zz <zzhe0042@student.monash.edu>
2022-09-20 00:38:41 +1000

    Update Alert Reminder to match new design; add progress bar; add take a break item

---
+++ src/components/ReminderItem.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import {defineProps, onUnmounted, ref, toRefs, watch } from 'vue'
+import Toggle from '@vueform/toggle'
+import '@vueform/toggle/themes/default.css'
+
+export interface ReminderConfig {
+  name: string
+  content: string
+  options: number[]
+  period: number
+  toggle: boolean
+  title: string
+  body: (mins: number) => string
+}
+
+const props = defineProps<{
+  config: ReminderConfig
+}>()
+const {config} = toRefs(props)
+
+const intervalId = ref(0)
+const updateIntervalId = ref(0)
+const startTime = ref(0)
+const progress = ref(0)
+
+watch([()=>config.value.period, ()=>config.value.toggle], ([period, toggle]) => {
+  clearInterval(intervalId.value)
+  clearInterval(updateIntervalId.value)
+  progress.value = 0
+  if (toggle) {
+    const ms = period * 60 * 1000
+    startTime.value = Date.now()
+    intervalId.value = setInterval(() => {
+      new Notification(config.value.title, {body: config.value.body(period)})
+    }, ms)
+    updateIntervalId.value = setInterval(() => {
+      progress.value = (Date.now() - startTime.value)%(config.value.period * 60 * 1000)
+    }, 300)
+    Notification.requestPermission()
+  }
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId.value)
+  clearInterval(updateIntervalId.value)
+})
+</script>
+    
+<template>
+  <div class="flex flex-col gap-4 p-8 bg-white">
+    <div class="flex">
+      <h3 class="font-sans text-2xl">
+        {{config.title}}
+      </h3>
+      <Toggle class="ml-auto" v-model="config.toggle"></Toggle>
+    </div>
+    <div>
+      <span>
+        {{config.content}}
+      </span>
+      <select v-model="config.period">
+        <option v-for="option in config.options" :key="option" :value="option">
+          {{option}} Minutes
+        </option>
+      </select>
+    </div>
+    <progress :max="config.period * 60 * 1000" :value="progress"></progress>
+  </div>
+</template>
+    
+<style lang="postcss" scoped>
+progress{
+  border-width: revert;
+  border-color: revert;
+  border-style: revert;
+  width: 100%;
+}
+</style>
--- src/views/AlertReminderView.vue
+++ src/views/AlertReminderView.vue
@@ -1,52 +1,54 @@
 <script setup lang="ts">
-// @ts-nocheck
-import Toggle from '@vueform/toggle'
-import '@vueform/toggle/themes/default.css'
-import { ref, watch } from 'vue';
+import type { ReminderConfig } from '@/components/ReminderItem.vue';
+import { ref, watch, reactive } from 'vue';
+import ReminderItem from '@/components/ReminderItem.vue';
 
-const period = ref<string>('20')
-const toggle = ref<boolean>(false)
-let intervalId = 0
-// Notification.requestPermission()
-
-watch([period, toggle], () => {
-  clearInterval(intervalId)
-  if (toggle.value){
-    const ms = parseInt(period.value) * 60 * 1000
-    intervalId = setInterval(sendNotification, ms)
-    Notification.requestPermission()
+const configs = reactive<ReminderConfig[]>([
+  {
+    name: "Water Reminder",
+    content: "Remind you to drink water at every:",
+    options: [20, 40, 60],
+    period: 20,
+    toggle: false,
+    title: "Time to drink water",
+    body: mins => `Remind you to drink water at every ${mins} minutes`
+  },
+  {
+    name: "Take a break",
+    content: "Remind you to take break at every:",
+    options: [20, 50, 80],
+    period: 20,
+    toggle: false,
+    title: "Time to take break",
+    body: mins => `Remind you to take break at every ${mins} minutes`
   }
-})
+])
 
-function sendNotification(){
-  new Notification(
-    "Time to drink water", 
-    {body: `Remind you to drink water at every ${period.value} minutes`}
-  )
-}
 </script>
     
 <template>
-  <section class="flex flex-col items-center gap-16 my-16">
+  <section class="flex flex-col items-center gap-16">
+    <section class="mx-auto w-full h-60 bg-gray-200 flex justify-center">
+      <div class="font-sans text-4xl pt-20">
+        Alert Reminder
+      </div>
+    </section>
     <h1 class="text-4xl font-sans">
       Alert Reminder Settings
     </h1>
 
-    <ul class="w-[58.25rem]">
-      <li class="flex flex-row p-8 bg-white">
-        <span>
-          Remind you to drink water at every:
-        </span>
-        <select v-model="period">
-          <option value="20">20 Minutes</option>
-          <option value="40">40 Minutes</option>
-          <option value="60">60 Minutes</option>
-        </select>
-        <Toggle class="ml-auto" v-model="toggle"></Toggle>
-      </li>
-    </ul>
+    <div class="w-[58.25rem] flex flex-col gap-16">
+      <ReminderItem 
+      v-for="config in configs" :key="config.name" 
+      :config="config"
+      class="reminder-item"></ReminderItem>
+    </div>
   </section>
 </template>
     
 <style lang="postcss" scoped>
+.reminder-item {
+  box-shadow: 5px 20px 50px rgba(16, 112, 177, 0.2);
+  border-radius: 10px;
+}
 </style>
