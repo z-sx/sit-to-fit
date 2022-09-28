22f88ef8a3794085b970b5a5343c1bb651ff2729
zz <zzhe0042@student.monash.edu>
2022-09-29 08:51:12 +1000

    Clear physical ergonomics page

--- src/views/PhysicalErgonomicsView.vue
+++ src/views/PhysicalErgonomicsView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import JourneyLayout from "../components/JourneyLayout.vue";
 </script>
     
 <template>
-    <JourneyLayout></JourneyLayout>
+
 </template>
     
 <style lang="postcss" scoped>
