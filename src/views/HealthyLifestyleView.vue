6634659c6c3b23bcb395a30a675bb7de0178eb88
zz <zzhe0042@student.monash.edu>
2022-09-29 07:59:25 +1000

    Remake healthy lifestyle page; add images and icons;

--- src/views/HealthyLifestyleView.vue
+++ src/views/HealthyLifestyleView.vue
@@ -1,116 +1,64 @@
 <script setup lang="ts">
-// import { Carousel, Slide, Pagination } from 'vue3-carousel';
-// import 'vue3-carousel/dist/carousel.css';
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css';
 
-import imgA1 from '@/assets/HealthyLifestyleView/activities-1.png'
-import imgA2 from '@/assets/HealthyLifestyleView/activities-2.png'
-import imgA3 from '@/assets/HealthyLifestyleView/activities-3.png'
-import imgD1 from '@/assets/HealthyLifestyleView/diet-1.png'
-import imgD2 from '@/assets/HealthyLifestyleView/diet-2.png'
-import imgD3 from '@/assets/HealthyLifestyleView/diet-3.png'
+import activityImg from '@/assets/HealthyLifestyleView/activities.jpg'
+import dietImg from '@/assets/HealthyLifestyleView/diets.jpg'
+import ergonomicImg from '@/assets/HealthyLifestyleView/ergonomics.jpg'
 
-const activities = [
-  imgA1,
-  imgA2,
-  imgA3
-]
-const diets = [
-  imgD1,
-  imgD2,
-  imgD3,
-]
+const sections = [{
+  title: "Physical Activities",
+  desc: "Participating in regular physical activities is one of the best ways you can improve your overall physical health",
+  route: "physical-activities",
+  img: activityImg,
+},{
+  title: "Dietary Plans",
+  desc: "Following a good diet plan helps boost your metabolism and reduces fat.",
+  route: "dietary-plan",
+  img: dietImg,
+},{
+  title: "Physical Ergonomics",
+  desc: "Learn the importance of good posture to mitigate longterm effects of sedantary lifestyle",
+  route: "physical-ergonomics",
+  img: ergonomicImg,
+}]
+
 </script>
 <template>
-  <section class="flex flex-col justify-center gap-11 my-16">
-    <h1 class="text-4xl font-sans mx-auto">Physical activities you don’t want to miss!</h1>
-    <div class="max-w-[58.25rem] mx-auto">
-      <Swiper
-      :centeredSlides="true"
-      :autoplay="{
-        delay: 2500,
-        disableOnInteraction: false,
-      }"
-      :pagination="{
-        clickable: true,
-      }"
-      :navigation="true"
-      >
-        <SwiperSlide v-for="image, index in activities" :key="index">
-          <div>
-            <img :src="image" alt="Physical Activity">
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <!-- <Swiper
-        :modules="modules"
-        :slides-per-view="3"
-        :space-between="50"
-        navigation
-        :pagination="{ clickable: true }"
-        :scrollbar="{ draggable: true }"
-        @swiper="onSwiper"
-        @slideChange="onSlideChange"
-      >
-      <Carousel :autoplay="4000" :wrap-around="true">
-        <Slide v-for="image, index in activities" :key="index">
-          <div>
-            <img :src="image" alt="Physical Activity">
-          </div>
-        </Slide>
-        <template #addons>
-          <Pagination class="pagination" />
-        </template>
-      </Carousel> -->
+  <section class="mx-auto w-full h-44 bg-gray-200 flex justify-center items-center">
+    <div class="font-sans text-4xl">
+      Healthy Lifestyle Changes you don't want to miss!
     </div>
-    <span class="max-w-[46.125rem] mx-auto">
-      Participating in regular physical activities is one of the best ways you can improve your overall physical health.
-      Being involved with physical activities allows your body to improve its brain health, and weight management,
-      reduce the risk of diseases, and strengthen your bones and muscles. According to the World Health Organisation,
-      physical inactivity is one of the leading risk factors for noncommunicable disease mortality as adults who are
-      physically inactive have a 20% to 30% increased risk of death compared to adults who are living a physically
-      active lifestyle.
-    </span>
-    <RouterLink class="home-button" :to="{name: 'physical-activities'}">
-      Discover More
-      <img src="@/assets/icons/IconArrowRight.svg" alt="Right Icon">
-    </RouterLink>
   </section>
-  <section class="flex flex-col justify-center gap-11 my-16">
-    <h1 class="text-4xl font-sans mx-auto">Healthy diet is the way to go!</h1>
-    <div class="max-w-[58.25rem] mx-auto">
-      <Swiper
-      :centeredSlides="true"
-      :autoplay="{
-        delay: 2500,
-        disableOnInteraction: false,
-      }"
-      :pagination="{
-        clickable: true,
-      }"
-      :navigation="true"
-      >
-        <SwiperSlide v-for="image, index in diets" :key="index">
-          <div>
-            <img :src="image" alt="Physical Diet">
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
-    <span class="max-w-[46.125rem] mx-auto">
-      A balanced healthy diet can assist in preventing certain long-term dangerous diseases such as heart disease,
-      stroke, and type 2 diabetes. In addition, it may help reduce the risk of developing certain kinds of cancers and
-      help keep your body weight at a healthy level. It is essential to find the right balance between the different
-      nutrients your body needs to function to achieve maximum health benefits.
-    </span>
-    <RouterLink class="home-button" :to="{name: 'dietary-plan'}">
-      Discover More
-      <img src="@/assets/icons/IconArrowRight.svg" alt="Right Icon">
-    </RouterLink>
+  <section class="flex flex-col gap-8 max-w-screen-xl mx-auto p-14">
+    <template v-for="section in sections">
+      <div class="card-shadow p-16 rounded-xl">
+        <div class="aspect-[2/1] bg-cover bg-center p-10 flex flex-col justify-end gap-4" 
+        :style="{backgroundImage: `url(${section.img})`}">
+          <h2 class="font-sans font-bold text-4xl text-white">
+            {{section.title}}
+          </h2>
+          <span class="font-sans text-xl text-white">
+            {{section.desc}}
+          </span>
+          <RouterLink 
+          class="flex flex-row items-center py-1 pl-4 pr-2 h-14 gap-2 bg-blue-600 opacity-80 w-max border-2 border-transparent hover:border-white hover:opacity-100 transition"
+          :to="{name: section.route}">
+            <span class="font-sans text-white">
+              Discover More
+            </span>
+            <img src="@/assets/icons/IconArrowRightWhite.svg" alt="Arrow Right">
+          </RouterLink>
+        </div>
+      </div>
+    </template>
+
   </section>
 </template>
 <style lang="postcss" scoped>
+.card-shadow{
+  box-shadow: 5px 20px 50px rgba(16, 112, 177, 0.2);
+}
 .home-button {
   @apply self-center flex flex-row items-center py-1 px-4 gap-2 bg-cyan-200 border-2 border-white;
 }
