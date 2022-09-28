480f30dc79a26751b2485278ac0f0939a9b0f634
zz <zzhe0042@student.monash.edu>
2022-09-29 01:34:40 +1000

    Modify header based on new design

--- src/components/HeaderV2.vue
+++ src/components/HeaderV2.vue
@@ -18,29 +18,21 @@ const links = ref([
     name: 'Alert Reminder',
     to: '/alert-reminder',
   },
-  {
-    name: 'Notifications',
-    to: '/notifications',
-  },
 ])
 </script>
   
 <template>
-  <header class="z-10 relative max-w-screen-xl mx-auto top-0 left-0 right-0 flex flex-row justify-between align-center px-8 py-2 gap-12 h-20">
+  <header class="z-10 relative max-w-screen-xl mx-auto top-0 left-0 right-0 flex flex-row justify-between align-center px-8 py-2 gap-20 xl:gap-36 h-20">
     <RouterLink class="no-underline" to="/">
       <img alt="Logo" src="@/assets/Logo.svg" />
     </RouterLink>
-    <nav class="navigation flex flex-row justify-center items-center h-16 grow-0 gap-1">
+    <nav class="navigation grow flex flex-row justify-between items-center h-16 gap-1">
       <template v-for="link in links">
         <RouterLink class="button" :to="link.to">
           {{link.name}}
         </RouterLink>
       </template>
     </nav>
-    <RouterLink class="button " to="/become-new-user">
-      <img src="@/assets/icons/IconSmile.svg" alt="Smile Icon">
-      Become new user
-    </RouterLink>
   </header>
 </template>
     
