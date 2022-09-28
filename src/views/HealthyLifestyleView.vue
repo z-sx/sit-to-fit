99396828000a9625bb626ecc632cfa30e169f324
zz <zzhe0042@student.monash.edu>
2022-09-29 01:33:48 +1000

    fix carousels

--- src/views/HealthyLifestyleView.vue
+++ src/views/HealthyLifestyleView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { Carousel, Slide, Pagination } from 'vue3-carousel';
-import 'vue3-carousel/dist/carousel.css';
+// import { Carousel, Slide, Pagination } from 'vue3-carousel';
+// import 'vue3-carousel/dist/carousel.css';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css';
+
 import imgA1 from '@/assets/HealthyLifestyleView/activities-1.png'
 import imgA2 from '@/assets/HealthyLifestyleView/activities-2.png'
 import imgA3 from '@/assets/HealthyLifestyleView/activities-3.png'
@@ -23,6 +26,33 @@ const diets = [
   <section class="flex flex-col justify-center gap-11 my-16">
     <h1 class="text-4xl font-sans mx-auto">Physical activities you don’t want to miss!</h1>
     <div class="max-w-[58.25rem] mx-auto">
+      <Swiper
+      :centeredSlides="true"
+      :autoplay="{
+        delay: 2500,
+        disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      >
+        <SwiperSlide v-for="image, index in activities" :key="index">
+          <div>
+            <img :src="image" alt="Physical Activity">
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <!-- <Swiper
+        :modules="modules"
+        :slides-per-view="3"
+        :space-between="50"
+        navigation
+        :pagination="{ clickable: true }"
+        :scrollbar="{ draggable: true }"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
       <Carousel :autoplay="4000" :wrap-around="true">
         <Slide v-for="image, index in activities" :key="index">
           <div>
@@ -32,7 +62,7 @@ const diets = [
         <template #addons>
           <Pagination class="pagination" />
         </template>
-      </Carousel>
+      </Carousel> -->
     </div>
     <span class="max-w-[46.125rem] mx-auto">
       Participating in regular physical activities is one of the best ways you can improve your overall physical health.
@@ -50,16 +80,23 @@ const diets = [
   <section class="flex flex-col justify-center gap-11 my-16">
     <h1 class="text-4xl font-sans mx-auto">Healthy diet is the way to go!</h1>
     <div class="max-w-[58.25rem] mx-auto">
-      <Carousel :autoplay="4000" :wrap-around="true">
-        <Slide v-for="image, index in diets" :key="index">
+      <Swiper
+      :centeredSlides="true"
+      :autoplay="{
+        delay: 2500,
+        disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      >
+        <SwiperSlide v-for="image, index in diets" :key="index">
           <div>
-            <img :src="image" alt="Healthy Diet">
+            <img :src="image" alt="Physical Diet">
           </div>
-        </Slide>
-        <template #addons>
-          <Pagination class="pagination" />
-        </template>
-      </Carousel>
+        </SwiperSlide>
+      </Swiper>
     </div>
     <span class="max-w-[46.125rem] mx-auto">
       A balanced healthy diet can assist in preventing certain long-term dangerous diseases such as heart disease,
