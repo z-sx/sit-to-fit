b7ba0c7162a473fb86141a1e779e50a260cf4229
zz <zzhe0042@student.monash.edu>
2022-10-11 17:07:44 +1100

    Change font face to sans

--- src/views/HomeViewV2.vue
+++ src/views/HomeViewV2.vue
@@ -7,20 +7,20 @@
     <section class="relative z-0">
       <div class="h-[45rem] xl:h-[50rem] xl:max-w-screen-xl xl:mx-auto relative z-10 flex flex-col px-14 pt-36 justify-between">
         <div class="w-[50%]">
-          <div class="text-4xl font-bold font-serif">Sedentary Lifestyle?</div>
-          <div class="text-3xl font-normal font-serif">To the people of Melbourne CBD,</div>
-          <div class="text-3xl font-normal font-serif">We are here to help you change your lifestyle.</div>
+          <div class="text-4xl font-bold font-sans">Sedentary Lifestyle?</div>
+          <div class="text-3xl font-normal font-sans">To the people of Melbourne CBD,</div>
+          <div class="text-3xl font-normal font-sans">We are here to help you change your lifestyle.</div>
         </div>
         <div class="w-fit self-end py-8 gap-3 flex flex-col">
           <RouterLink class="rounded-2xl self-center flex flex-col pl-8 pr-16 pt-2 pb-5 text-white bg-blue-600 border-2 border-transparent opacity-95 hover:opacity-100 hover:border-white transition" :to="{name: 'journey'}">
-            <span class="font-serif text-3xl">
+            <span class="font-sans text-3xl">
               Start
             </span>
-            <span class="font-serif text-xl">
+            <span class="font-sans text-xl">
               your healthy lifestyle
             </span>
           </RouterLink>
-          <span class="font-serif text-2xl pl-10">
+          <span class="font-sans text-2xl pl-10">
             Break your sedentary lifestyle!
           </span>
         </div>
